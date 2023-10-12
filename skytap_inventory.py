6d95f0d45709178b50eedd38fcf554a1719eb725
Blagovest Petrov <blagovest@petrovs.info>
2023-10-13 01:50:37 +0300

    Some refactoring

--- skytap_inventory.py
+++ skytap_inventory.py
@@ -36,7 +36,6 @@ class Client(object):
     def __init__(self, base_url, username, password, **kwargs):
         """Initialize a client session"""
         self.session = requests.Session()
-        # self.session.mount("http://", HTTPAdapter(max_retries=kwargs.get("max_retries", 5)))
         self.session.mount("https://", HTTPAdapter(max_retries=kwargs.get("max_retries", 5)))
         self.session.auth = (username, password)
         self.session.verify = kwargs.get("ssl_cert_file", True)
@@ -140,53 +139,44 @@ class SkytapInventory(object):
     def read_settings(self, override_config_file=None):
         config = configparser.ConfigParser(allow_no_value=True)
 
-        # default looks for skytap.ini in the current working directory; can be over-ridden by $SKYTAP_INI env variable
-        # config filename can be over-ridden with function argument (expected use: unit testing)
-        config_filename = "skytap.ini"
-        if override_config_file:
-            config_filename = override_config_file
+        # Determine the config filename
+        config_filename = override_config_file or "skytap.ini"
         skytap_default_ini_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), config_filename)
         skytap_ini_path = os.path.expanduser(os.path.expandvars(os.environ.get("SKYTAP_INI", skytap_default_ini_path)))
         config.read(skytap_ini_path)
 
-        # config values are set as side effects in three places: skytap_vars, skytap_env_vars, and ansible_config_vars
-        # tests should validate the state of these three objects.
-        # these are required args; "None" indicates no CLI args were present
-        if self.skytap_vars["username"] is None:
-            self.skytap_vars["username"] = config.get("skytap_vars", "username")
-        if self.skytap_vars["api_token"] is None:
-           self.skytap_vars["api_token"] = config.get("skytap_vars", "api_token")
-        if self.skytap_env_vars["configuration_id"] is None:
-           self.skytap_env_vars["configuration_id"] = config.get("skytap_env_vars", "configuration_id")
-        # defaults are set in __init__; config may over-ride 
-        if config.has_option("skytap_env_vars", "network_type"):
-           self.skytap_env_vars["network_type"] = config.get("skytap_env_vars", "network_type")
-        if config.has_option("skytap_env_vars", "network_connection_id"):
-            self.skytap_env_vars["network_connection_id"] = config.get("skytap_env_vars", "network_connection_id")
-        if config.has_option("skytap_vars", "base_url"):
-            self.skytap_vars["base_url"] = config.get("skytap_vars", "base_url")
-        # these vars are used to set ssh credentials from the 'credentials' field in the Skytap API response for VM's
-        if config.has_option('skytap_env_vars', 'use_api_credentials'):
-            value = config.get('skytap_env_vars', 'use_api_credentials').upper()
-        if value == 'TRUE':
-            self.skytap_env_vars['use_api_credentials'] = True
-        if config.has_option('skytap_env_vars', 'skytap_vm_username'):
-            self.skytap_env_vars['skytap_vm_username'] = config.get('skytap_env_vars', 'skytap_vm_username')
-        if config.has_option('skytap_env_vars', 'api_credential_delimiter'):
-            self.skytap_env_vars['api_credential_delimiter'] = config.get('skytap_env_vars', 'api_credential_delimiter')
-        # skytap.ini may over-ride ansible variables  
-        if config.has_option("ansible_ssh_vars", "user"):
-            self.ansible_config_vars["ansible_ssh_user"] = config.get("ansible_ssh_vars", "user")
-        if config.has_option("ansible_ssh_vars", "port"):
-            self.ansible_config_vars["ansible_ssh_port"] = config.get("ansible_ssh_vars", "port")
-        if config.has_option("ansible_ssh_vars", "pass"):
-            self.ansible_config_vars["ansible_ssh_pass"] = config.get("ansible_ssh_vars", "pass")
-        if config.has_option("ansible_ssh_vars", "host"):
-            self.ansible_config_vars["ansible_ssh_host"] = config.get("ansible_ssh_vars", "host")
-        if config.has_option("ansible_ssh_vars", "private_key_file"):
-            self.ansible_config_vars["ansible_ssh_private_key_file"] = config.get("ansible_ssh_vars", "private_key_file")
-        # set ansible vars in inventory object
+        # Helper function to get value from environment or config
+        def get_value(vars_dict, key, section=None, env_prefix="SKYTAP_"):
+            env_val = os.environ.get(env_prefix + key.upper())
+            if env_val:
+                return env_val
+            if section and config.has_option(section, key):
+                return config.get(section, key)
+            return vars_dict[key]
+
+        # Update skytap_vars
+        for key in self._skytap_vars:
+            self._skytap_vars[key] = get_value(self._skytap_vars, key, "skytap_vars")
+
+        # Update skytap_env_vars
+        for key in self._skytap_env_vars:
+            self._skytap_env_vars[key] = get_value(self._skytap_env_vars, key, "skytap_env_vars")
+
+        # Update ansible_config_vars
+        ansible_config_mapping = {
+            "user": "ansible_ssh_user",
+            "port": "ansible_ssh_port",
+            "pass": "ansible_ssh_pass",
+            "host": "ansible_ssh_host",
+            "private_key_file": "ansible_ssh_private_key_file"
+        }
+        for key, mapped_key in ansible_config_mapping.items():
+            if config.has_option("ansible_ssh_vars", key):
+                self._ansible_config_vars[mapped_key] = config.get("ansible_ssh_vars", key)
+
+        # Set ansible vars in inventory object
         self._inventory_template["skytap_environment"]["vars"] = self._ansible_config_vars
+
 
     def get_data(self):
         query_string = f"{RESOURCE_NAME}/{self.skytap_env_vars['configuration_id']}.json"
@@ -201,84 +191,62 @@ class SkytapInventory(object):
 
     def parse_credentials_for_vm(self, vm_data):
         user_pass = {}
-        if not self.skytap_env_vars['use_api_credentials']:
-            return user_pass
-        if not vm_data['credentials']:
+
+        if not self.skytap_env_vars['use_api_credentials'] or not vm_data.get('credentials'):
             return user_pass
 
-        # local shortnames
         l_delim = self.skytap_env_vars['api_credential_delimiter']
         l_uname = self.skytap_env_vars['skytap_vm_username']
 
-        # if there is a single credential pair and username is unset, use the pair available
-        if len(vm_data['credentials']) == 1 and l_uname is None:
-            selected_creds = vm_data['credentials']
-        else:
-            # credentials object is a list of dictionaries; each dictionary contains a field called 'text'. We're interested in 
-            # the first token of the 'text' field when the field is split on some delimiter (e.g., {'text': 'username / password'})
-            selected_creds = list(filter(lambda cred_obj: cred_obj['text'].split(l_delim)[0].strip() == l_uname, vm_data['credentials']))
+        # Filter credentials based on provided username or select the first one if username is not provided
+        selected_creds = next((cred for cred in vm_data['credentials'] if l_uname and cred['text'].split(l_delim)[0].strip() == l_uname), vm_data['credentials'][0])
 
-        if not selected_creds:
-            return user_pass  # no match; return empty dict
-        else:
-            selected_creds = selected_creds[0]['text']
-
-        # now split the selected_creds string into a dictionary that we can merge with the inventory data structure
-        user_pass['ansible_ssh_user'] = selected_creds.split(l_delim)[0].strip()
-        user_pass['ansible_ssh_pass'] = selected_creds.split(l_delim)[1].strip()
+        # Extract user and password from the selected credentials
+        user, _, password = selected_creds['text'].partition(l_delim)
+        user_pass['ansible_ssh_user'] = user.strip()
+        user_pass['ansible_ssh_pass'] = password.strip()
 
         return user_pass
+
+    def add_host_to_inventory(self, inventory, interface, ip_address, creds_dict):
+        hostname = str(interface["hostname"])
+        inventory["skytap_environment"]["hosts"].append(hostname)
+        inventory["_meta"]["hostvars"][hostname] = {"ansible_ssh_host": ip_address}
+        inventory["_meta"]['hostvars'][hostname].update(creds_dict)
+
     
     def build_private_ip_group(self, client_data, inventory):
         for vm in client_data["vms"]:
             creds_dict = self.parse_credentials_for_vm(vm)
-            for interface in vm["interfaces"]: 
-                if "ip" in interface and interface["ip"] is not None:
-                    hostname = str(interface["hostname"])
-                    inventory["skytap_environment"]["hosts"].append(hostname)
-                    inventory["_meta"]["hostvars"][hostname] = {"ansible_ssh_host": str(interface["ip"])}
-                    inventory["_meta"]['hostvars'][hostname].update(creds_dict)
+            for interface in vm["interfaces"]:
+                if "ip" in interface and interface["ip"]:
+                    self.add_host_to_inventory(inventory, interface, str(interface["ip"]), creds_dict)
         return inventory
     
-    def build_icnr_ip_group(self, client_data, inventory): 
-        """update the inventory file to include a group for the ICNR IP addresses"""
+    def build_icnr_ip_group(self, client_data, inventory):
         tunnel_source_network = None
         if self.skytap_env_vars["network_connection_id"]:
-            matching_tunnels = [ tunnel for tunnel in client_data["tunnels"] if tunnel["id"] == self.skytap_env_vars["network_connection_id"] ]
-            if not matching_tunnels:
-                raise Exception("No tunnels with id %s found" % self.skytap_env_vars["network_connection_id"])
+            matching_tunnels = [tunnel for tunnel in client_data["tunnels"] if tunnel["id"] == self.skytap_env_vars["network_connection_id"]]
+            if matching_tunnels:
+                tunnel_source_network = matching_tunnels[0]["source_network"]["url"]
 
-            tunnel_source_network = matching_tunnels[0]["source_network"]["url"]
-
-        for vm in client_data["vms"]: 
-            creds_dict = self.parse_credentials_for_vm(vm)
-            for interface in vm["interfaces"]:
-                if "nat_addresses" in interface and "network_nat_addresses" in interface["nat_addresses"]:  
-                    for network_nat in interface["nat_addresses"]["network_nat_addresses"]:
-                        if tunnel_source_network and network_nat["network_url"] != tunnel_source_network:
-                            continue
-                        hostname = str(interface["hostname"])
-                        inventory["skytap_environment"]["hosts"].append(hostname)
-                        inventory["_meta"]["hostvars"][hostname] = {"ansible_ssh_host": str(network_nat["ip_address"])} 
-                        inventory["_meta"]['hostvars'][hostname].update(creds_dict)
-        return inventory
-    
-    def build_vpn_ip_group(self, client_data, inventory): 
-        """update the inventory file to include a group for the VPN IP addresses"""
         for vm in client_data["vms"]:
             creds_dict = self.parse_credentials_for_vm(vm)
             for interface in vm["interfaces"]:
-                if "nat_addresses" in interface and "vpn_nat_addresses" in interface["nat_addresses"]:
-                    for vpn_nat in interface["nat_addresses"]["vpn_nat_addresses"]:
-                        if self.skytap_env_vars["network_connection_id"] and self.skytap_env_vars["network_connection_id"] != vpn_nat["vpn_id"]:
-                            continue
-                        hostname = str(interface["hostname"])
-                        inventory["skytap_environment"]["hosts"].append(hostname)
-                        inventory["_meta"]["hostvars"][hostname] = {"ansible_ssh_host": str(vpn_nat["ip_address"])}
-                        inventory["_meta"]['hostvars'][hostname].update(creds_dict)
-                        
-                        # just hostname/nat_vpn per interface
-                        break
+                if "nat_addresses" in interface:
+                    for network_nat in interface["nat_addresses"].get("network_nat_addresses", []):
+                        if not tunnel_source_network or network_nat["network_url"] == tunnel_source_network:
+                            self.add_host_to_inventory(inventory, interface, str(network_nat["ip_address"]), creds_dict)
+        return inventory
+    
+    def build_vpn_ip_group(self, client_data, inventory):
+        for vm in client_data["vms"]:
+            creds_dict = self.parse_credentials_for_vm(vm)
+            for interface in vm["interfaces"]:
+                if "nat_addresses" in interface:
+                    for vpn_nat in interface["nat_addresses"].get("vpn_nat_addresses", []):
+                        if not self.skytap_env_vars["network_connection_id"] or self.skytap_env_vars["network_connection_id"] == vpn_nat["vpn_id"]:
+                            self.add_host_to_inventory(inventory, interface, str(vpn_nat["ip_address"]), creds_dict)
         return inventory
     
     def get_inventory(self):
